eeaeaa054e68700df31f4ed2c112b816fb53817a
Franco <francocelan2016@gmail.com>
2022-04-22 15:11:30 -0300

    Version con GIT octa

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Funciona Octa
+          Version con solo GIT
         </p>
         <a
           className="App-link"
