cf73323ef644a60ad749b6ad7b92670d695efbf7
Franco <francocelan2016@gmail.com>
2022-04-22 15:13:25 -0300

    Version Completa

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Version con solo GIT
+          Version con solo FINAL
         </p>
         <a
           className="App-link"
